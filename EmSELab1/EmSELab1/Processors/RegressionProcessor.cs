bd3c831a41896143ef575efbf799233533013aa7
VladyslavBiletskyi <vladyslav.biletskyi@nure.ua>
2017-10-22 21:42:16 +0300

    FitPoints rewritten

--- EmSELab1/EmSELab1/Processors/RegressionProcessor.cs
+++ EmSELab1/EmSELab1/Processors/RegressionProcessor.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using MathNet.Numerics.LinearRegression;
 namespace EmSELab1.Processors
 {
     public class RegressionProcessor
     {
         public static Tuple<double, double> FitPoints(float[] xs, float[] ys)
         {
-            return SimpleRegression.Fit(Array.ConvertAll(xs, x => (double)x), Array.ConvertAll(ys, y => (double)y));
+            return MathNet.Numerics.Fit.Line(Array.ConvertAll(xs, x => (double)x), Array.ConvertAll(ys, y => (double)y));
         }
     }
 }
