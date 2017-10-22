bb87f785365572bc1cc6a6b852a067c241fcd7a0
VladyslavBiletskyi <vladyslav.biletskyi@nure.ua>
2017-10-22 13:22:38 +0300

    Project init

---
+++ EmSELab1/EmSELab1/DTOs/InputDataDto.cs
@@ -0,0 +1,11 @@
+﻿namespace EmSELab1.DTOs
+{
+    public class InputDataDto
+    {
+        public int Y { get; set; }
+
+        public int X1 { get; set; }
+
+        public int X2 { get; set; }
+    }
+}
