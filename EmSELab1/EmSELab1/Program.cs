2eae4ca1cbc147e75efb5a78ea8d6b73f4fcf4f5
Eugene <nickiitee@gmail.com>
2017-10-22 14:17:54 +0300

    test

--- EmSELab1/EmSELab1/Program.cs
+++ EmSELab1/EmSELab1/Program.cs
@@ -14,6 +14,7 @@ namespace EmSELab1
         [STAThread]
         static void Main()
         {
+            //222222
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
